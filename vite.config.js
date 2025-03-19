1de9ec9c9d7fba0689a385ebf3259660bc4782cc
SHIVA-THARU <chaudharyshiva8848@gmail.com>
2025-03-19 15:18:42 +0530

    Fixed GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "gym-website", // Replace "gym-website" with your repository name
   plugins: [react()],
+  base: "/gym-website/",
 });
